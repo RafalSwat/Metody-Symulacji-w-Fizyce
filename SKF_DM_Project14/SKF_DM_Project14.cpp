e36afc1b538b723618e005a26210849cd188209a
RafalSwat <37617768+RafalSwat@users.noreply.github.com>
2018-05-25 12:32:11 +0200

    Update SKF_DM_Project14.cpp

--- SKF_DM_Project14/SKF_DM_Project14.cpp
+++ SKF_DM_Project14/SKF_DM_Project14.cpp
@@ -1,19 +1,126 @@
-#include<iostream>
+// SKF_DM_project_14.cpp : Defines the entry point for the console application.
+//
+
+//#include "stdafx.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
 #include <random>
-#include <cmath>
-#include <fstream>
-#include <SFML\Graphics.hpp>
+#include <list>
+#include <array>
+#include "brown_motion.h"
+#include "event.h"
 
+//std::mt19937 generator(123);
+//std::uniform_real_distribution<double> dis(0.0, 1.0);
 
+int static const SIZE = 600;
+int static const N = 2;
+
+void DrawParticle(Particle const& p, sf::CircleShape & shape, sf::RenderWindow & window)
+{
+	auto const pos = p.getPosition();
+	auto const radius = SIZE * p.getRadius();
+	shape.setPosition(pos.x * SIZE - radius, pos.y * SIZE - radius);
+	shape.setRadius(SIZE * p.getRadius());
+	window.draw(shape);
+}
+
+void DrawParticles(std::array<Particle*, N> & particles, sf::CircleShape & shape, sf::RenderWindow & window)
+{
+	window.clear();
+	for (auto & elem : particles)
+	{
+		DrawParticle(*elem, shape, window);
+	}
+	window.display();
+}
+
+void InsertEvent(Event const& event, std::list<Event>& events)
+{
+	for (auto eventIt = events.begin(); eventIt != events.end(); eventIt++)
+	{
+		if (eventIt->getTime() > event.getTime())
+		{
+			events.insert(eventIt, event);
+			return;
+		}
+	}
+	events.push_back(event);
+}
+
+void InvalidateRelatedEvents(Event const& executedEvent, std::list<Event> & events)
+{
+	std::list<Event>::iterator eventIt = events.begin();
+	while (eventIt != events.end())
+	{
+		if (eventIt->containParticle(executedEvent.getA()) || eventIt->containParticle(executedEvent.getB()))
+		{
+			eventIt = events.erase(eventIt);
+		}
+	}
+}
+
+void CheckParticles(Particle* a, Particle* b, std::list<Event>& events)
+{
+	auto const adtX = a->collidesX();
+	auto const adtY = a->collidesY();
+
+	auto const bdtX = b->collidesX();
+	auto const bdtY = b->collidesY();
+
+	auto const dtCollide = a->collides(*b);
+
+	if (dtCollide > 0.
+		&& dtCollide < adtX
+		&& dtCollide < adtY
+		&& dtCollide < bdtX
+		&& dtCollide < bdtY)
+	{
+		Event newEvent(Event::TYPE::COLLISION, dtCollide, a, b);
+		InsertEvent(newEvent, events);
+	}
+	else
+	{
+		auto const aEventType = (adtX < adtY ? Event::TYPE::AXIS_X : Event::TYPE::AXIS_Y);
+		Event const aNewEvent(aEventType, adtX < adtY ? adtX : adtY, a, nullptr);
+		InsertEvent(aNewEvent, events);
+
+		auto const bEventType = (bdtX < bdtY ? Event::TYPE::AXIS_X : Event::TYPE::AXIS_Y);
+		Event const bNewEvent(bEventType, bdtX < bdtY ? bdtX : bdtY, b, nullptr);
+		InsertEvent(bNewEvent, events);
+	}
+}
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
+	sf::RenderWindow window(sf::VideoMode(SIZE, SIZE), "SFML works!");
+	sf::CircleShape shape((float)SIZE);
 	shape.setFillColor(sf::Color::Green);
 
+	Particle a(0.02, 0.1, 0.2, 0.2, 0.01, -0.02);
+	Particle b(0.02, 0.1, 0.2, 0.7, 0.01, 0.02);
+
+	std::array<Particle*, N> particles;
+	particles[0] = &a;
+	particles[1] = &b;
+
+	std::list<Event> events;
+
+	for (size_t i = 0; i < N; ++i)
+	{
+		for (size_t j = i + 1; j < N; ++j)
+		{
+			auto a = particles[i];
+			auto b = particles[j];
+
+			CheckParticles(a, b, events);
+		}
+	}
+
+	double dt = 0.01;
 	while (window.isOpen())
 	{
+
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -21,12 +128,48 @@ int main()
 				window.close();
 		}
 
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
+		Event executedEvent;
+		if (!events.empty())
+		{
+			auto event = events.front();
+			if ((event.getTime() - dt) < 0.)
+			{
+				event.execute();
+				executedEvent = events.front();
+				events.pop_front();
+			}
+		}
 
-	
-	system("pause");
+		if (executedEvent.getValid())
+		{
+			InvalidateRelatedEvents(executedEvent, events);
+
+			auto a = executedEvent.getA();
+			auto b = executedEvent.getB();
+			for (auto particle : particles)
+			{
+				if (a != nullptr && a != particle)
+				{
+					CheckParticles(a, particle, events);
+				}
+
+				if (b != nullptr && b != particle)
+				{
+					CheckParticles(b, particle, events);
+				}
+			}
+		}
+
+		for (auto & elem : particles)
+		{
+			elem->move(dt);
+		}
+		for (auto & elem : events)
+		{
+			elem.updateTime(-dt);
+		}
+
+		DrawParticles(particles, shape, window);
+	}
 	return 0;
 }
