947215c0b66f48f05a023d85d6e226b6b2c3b292
RafalSwat <37617768+RafalSwat@users.noreply.github.com>
2018-05-25 12:32:47 +0200

    Update Project14.h

--- SKF_DM_Project14/Project14.h
+++ SKF_DM_Project14/Project14.h
@@ -1,52 +1,43 @@
-  #include<iostream>
-#include <random>
-#include <cmath>
+#ifndef BROWN_MOTION_H__
+#define BROWN_MOTION_H__
 
-class Coordinates
-{
-private:
-	double x;
-	double y;
-public:
-	Coordinates() : x(0), y(0) {}
-	Coordinates(double xx, double yy) : x(xx), y(yy) {}
+#include <SFML/Graphics.hpp>
+#include <iostream>
 
-	bool operator==(Coordinates & p);
-	Coordinates operator+(Coordinates & p);
-	Coordinates operator-(Coordinates & p);
-	Coordinates operator*(double & p);
-	Coordinates operator/(double & p);
-	double operator*(Coordinates & p);
-};
-
-class Box
-{
-private:
-	Coordinates left_down;
-	Coordinates right_up;
-public:
-	Box() : left_down(0, 0), right_up(100, 100) {}
-	Box(Coordinates l_d, Coordinates r_u) : left_down(l_d), right_up(r_u) {}
-};
 class Particle
 {
 private:
-	double mass;
-	double diameter;
 	double radius;
-	Coordinates position;
-	Coordinates velocity;
+	double mass;
+	double rx;
+	double ry;
+	double vx;
+	double vy;
+
+	int collisionCount;
 
 public:
-	Particle(): mass(1),  diameter(1), radius(1), position(0, 0), velocity(0, 0) {}
-	Particle(double m, double d, double r, Coordinates p, Coordinates v) : mass(m), diameter(d), radius(r), velocity(v) {}
+	Particle() : radius(1), mass(1), rx(0), ry(0), vx(0), vy(0), collisionCount(0) {}
+	Particle(double r, double m, double r_x, double r_y, double v_x, double v_y) 
+		:radius(r), mass(m), rx(r_x), ry(r_y), vx(v_x), vy(v_y), collisionCount(0) {}
 
-	double collidesX();
-	double collidesY();
-	double collides(Particle b);
+	double collidesX() const;
+	double collidesY() const;
+	double collides(Particle const& b) const;
+
 	void bounceX();
 	void bounceY();
-	void bounce(Particle b);
+	void bounce(Particle& b);
+
 	int getCollisionCount();
 
+	void move(double dt);
+
+	sf::Vector2f getPosition() const { return sf::Vector2f((float)rx, (float)ry); };
+	float getRadius() const { return (float)radius; }
+
+
 };
+
+#endif // !BROWN_MOTION_H__
+
