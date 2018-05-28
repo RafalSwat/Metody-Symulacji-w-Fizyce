dff7d67211e1306688277e947bdc8dc4d059d064
RafalSwat <37617768+RafalSwat@users.noreply.github.com>
2018-05-28 11:54:13 +0200

    Update brownian_motion.h

--- SKF_DM_Project14/brownian_motion.h
+++ SKF_DM_Project14/brownian_motion.h
@@ -3,6 +3,7 @@
 
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <random>
 
 class Particle
 {
@@ -18,16 +19,18 @@ private:
 
 public:
 
-	Particle() 
-		: radius(0.02), mass(0.1), rx(0), ry(0), vx(0.02), vy(0.02), color(sf::Color::Green), collisionCount(0) {}
-	Particle(double r_x, double r_y)
-		: radius(0.02), mass(0.1), rx(r_x), ry(r_y), vx(0.02), vy(0.02), color(sf::Color::Green), collisionCount(0) {}
+	Particle()
+		: radius(0.02), mass(0.01), rx(0), ry(0), vx(0.002), vy(0.002), color(sf::Color::Green), collisionCount(0) {}
+	Particle(double r_x, double r_y, double v_x, double v_y)
+		: radius(0.02), mass(0.1), rx(r_x), ry(r_y), vx(v_x), vy(v_y), color(sf::Color::Green), collisionCount(0) {}
 	Particle(double r, double m, double r_x, double r_y, double v_x, double v_y, const sf::Color &Color)
 		:radius(r), mass(m), rx(r_x), ry(r_y), vx(v_x), vy(v_y), color(Color), collisionCount(0) {}
 
 	double collidesX() const;
 	double collidesY() const;
 	double collides(Particle const& b) const;
+	double getX() { return rx; }
+	double getY() { return ry; }
 
 	void bounceX();
 	void bounceY();
